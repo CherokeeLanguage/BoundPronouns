bca64a38d42ada3a3cf9cc5c220dcdb6c797af3f
mjoyner <mjoyner@vbservices.net>
2015-01-10 12:18:35 -0500

    initial checkin

---
+++ desktop/src/com/cherokeelessons/bp/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.cherokeelessons.bp.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.cherokeelessons.bp.BoundPronouns;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BoundPronouns(), config);
+	}
+}
