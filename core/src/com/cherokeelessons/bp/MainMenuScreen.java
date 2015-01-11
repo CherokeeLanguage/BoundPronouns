9f571003141a52050751bdde924c8d06e5636507
mjoyner <mjoyner@vbservices.net>
2015-01-11 18:40:30 -0500

    "black bars"

--- core/src/com/cherokeelessons/bp/MainMenuScreen.java
+++ core/src/com/cherokeelessons/bp/MainMenuScreen.java
@@ -171,7 +171,7 @@ public class MainMenuScreen implements Screen {
 	@Override
 	public void render(float delta) {
 		stage.act();
-		Gdx.gl.glClearColor(1, 1, 1, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		stage.draw();
 	}
