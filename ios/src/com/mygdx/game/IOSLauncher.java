d6e02774d6e1e0267d013da9c798c311fcb8d5fc
mjoyner <mjoyner@vbservices.net>
2015-02-05 21:59:35 -0500

    manually add ios project to gradle setup and hope for the best

---
+++ ios/src/com/mygdx/game/IOSLauncher.java
@@ -0,0 +1,25 @@
+package com.mygdx.game;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.cherokeelessons.bp.BoundPronouns;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        config.allowIpod=true;
+        config.orientationLandscape=true;
+        config.orientationPortrait=false;
+        return new IOSApplication(new BoundPronouns(), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
