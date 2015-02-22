d7c7c582c29185649ee6a3a3236d815cd7e7034f
mjoyner <mjoyner@vbservices.net>
2015-02-22 10:38:46 -0500

    xxx

--- desktop/src/com/cherokeelessons/bp/desktop/DesktopLauncher.java
+++ desktop/src/com/cherokeelessons/bp/desktop/DesktopLauncher.java
@@ -212,12 +212,12 @@ public class DesktopLauncher implements PlatformTextInput, FBShareStatistics {
 			str.append(URLEncoder.encode("http://www.cherokeelessons.com/phpBB3/viewtopic.php?f=24&t=73#p230", "UTF-8"));
 			str.append("&picture=");
 			str.append(URLEncoder.encode("http://www.cherokeelessons.com/phpBB3/download/file.php?id=242", "UTF-8"));
-			str.append("&name=");
-			str.append(URLEncoder.encode("Cherokee Language Bound Pronouns", "UTF-8"));
 			str.append("&caption=");
 			str.append(URLEncoder.encode("Level: "+info.level.getLevel()+" - "+info.level, "UTF-8"));
 			str.append("&description=");
 			str.append(URLEncoder.encode(text, "UTF-8"));
+			str.append("&name=");
+			str.append(URLEncoder.encode("Cherokee Language Bound Pronouns", "UTF-8"));
 		} catch (UnsupportedEncodingException e1) {
 			return;
 		}
