6f14a1adb69909befb77608eb443ba55fad34580
mjoyner <mjoyner@vbservices.net>
2015-02-22 14:45:26 -0500

    xxx

--- android/src/com/cherokeelessons/bp/android/AndroidLauncher.java
+++ android/src/com/cherokeelessons/bp/android/AndroidLauncher.java
@@ -19,7 +19,7 @@ public class AndroidLauncher extends AndroidApplication implements
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-//		BoundPronouns.fb=this;
+		BoundPronouns.fb = this;
 		BoundPronouns game = new BoundPronouns();
 		initialize(game, config);
 	}
@@ -36,37 +36,21 @@ public class AndroidLauncher extends AndroidApplication implements
 				+ "% medium term memorized";
 		text += ", " + ((int) (info.longTerm * 100)) + "% fully learned";
 		StringBuilder str = new StringBuilder();
-		try {
-			str.append("https://www.facebook.com/dialog/feed?");
-			str.append("&app_id=");
-			str.append("148519351857873");
-			str.append("&redirect_uri=");
-			str.append(URLEncoder
-					.encode("http://www.cherokeelessons.com/phpBB3/viewforum.php?f=24#",
-							"UTF-8"));
-			str.append("&link=");
-			str.append(URLEncoder
-					.encode("http://www.cherokeelessons.com/phpBB3/viewtopic.php?f=24&t=73#p230",
-							"UTF-8"));
-			str.append("&picture=");
-			str.append(URLEncoder
-					.encode("http://www.cherokeelessons.com/phpBB3/download/file.php?id=242",
-							"UTF-8"));
-			str.append("&name=");
-			str.append(URLEncoder.encode("Cherokee Language Bound Pronouns",
-					"UTF-8"));
-			str.append("&caption=");
-			str.append(URLEncoder.encode("Level: " + info.level.getLevel()
-					+ " - " + info.level, "UTF-8"));
-			str.append("&description=");
-			str.append(URLEncoder.encode(text, "UTF-8"));
-		} catch (UnsupportedEncodingException e1) {
-			e1.printStackTrace();
-			return;
-		}
-		
-		Uri uri = Uri.parse(str.toString());
-		Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-		startActivity(intent);
+
+		str.append("Cherokee Language Bound Pronouns\n");
+		str.append("Level: ");
+		str.append(info.level.getLevel());
+		str.append(" - ");
+		str.append(info.level);
+		str.append("\n");
+		str.append(text);
+		str.append("\n");
+
+		Intent sendIntent = new Intent();
+		sendIntent.setAction(Intent.ACTION_SEND);
+		sendIntent.putExtra(Intent.EXTRA_TEXT, str.toString());
+		sendIntent.setType("text/plain");
+		startActivity(Intent.createChooser(sendIntent,
+				"Share My Statistics ..."));
 	}
 }
