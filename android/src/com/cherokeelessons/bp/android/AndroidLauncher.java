375a78aa410e14634ce18a5a7a73e2a990dcd737
mjoyner <mjoyner@vbservices.net>
2015-02-22 17:06:07 -0500

    share via link

--- android/src/com/cherokeelessons/bp/android/AndroidLauncher.java
+++ android/src/com/cherokeelessons/bp/android/AndroidLauncher.java
@@ -1,10 +1,6 @@
 package com.cherokeelessons.bp.android;
 
-import java.awt.Desktop;
-import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.net.URLEncoder;
 
 import android.content.Intent;
