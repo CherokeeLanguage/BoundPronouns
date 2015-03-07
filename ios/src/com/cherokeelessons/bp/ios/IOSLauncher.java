a2506cbd2c307d1ebf6185aa1f93286295742605
mjoyner <mjoyner@vbservices.net>
2015-03-07 09:16:14 -0500

    bind gservices to ios app

--- ios/src/com/cherokeelessons/bp/ios/IOSLauncher.java
+++ ios/src/com/cherokeelessons/bp/ios/IOSLauncher.java
@@ -6,10 +6,12 @@ import org.robovm.apple.uikit.UIApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
 import com.cherokeelessons.bp.BoundPronouns;
+import com.cherokeelessons.play.GameServices;
 
 public class IOSLauncher extends IOSApplication.Delegate {
     @Override
     protected IOSApplication createApplication() {
+    	BoundPronouns.services=new GameServices(new Platform());
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
         config.allowIpod=true;
         config.orientationLandscape=true;
@@ -21,5 +23,5 @@ public class IOSLauncher extends IOSApplication.Delegate {
         NSAutoreleasePool pool = new NSAutoreleasePool();
         UIApplication.main(argv, null, IOSLauncher.class);
         pool.close();
-    }    
+    }
 }
