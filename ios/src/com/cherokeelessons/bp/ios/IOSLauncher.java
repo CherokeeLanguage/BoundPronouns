68e43f48352560d1f89d648fe09c67d4aa9b298a
mjoyner <mjoyner@vbservices.net>
2015-03-07 11:47:26 -0500

    ios webview working

--- ios/src/com/cherokeelessons/bp/ios/IOSLauncher.java
+++ ios/src/com/cherokeelessons/bp/ios/IOSLauncher.java
@@ -1,6 +1,8 @@
 package com.cherokeelessons.bp.ios;
 
 import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.foundation.NSPropertyList;
+import org.robovm.apple.foundation.NSURL;
 import org.robovm.apple.uikit.UIApplication;
 
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
@@ -9,14 +11,24 @@ import com.cherokeelessons.bp.BoundPronouns;
 import com.cherokeelessons.play.GameServices;
 
 public class IOSLauncher extends IOSApplication.Delegate {
+	Platform platform;
     @Override
     protected IOSApplication createApplication() {
-    	BoundPronouns.services=new GameServices(new Platform());
+    		platform = new Platform();
+		BoundPronouns.services=new GameServices(platform);
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
         config.allowIpod=true;
         config.orientationLandscape=true;
         config.orientationPortrait=false;
         return new IOSApplication(new BoundPronouns(), config);
+    }
+    
+    @Override
+    public boolean openURL(UIApplication application, NSURL url,
+    		String sourceApplication, NSPropertyList annotation) {
+    		platform.webview(url);
+    		return true;
+//    		return super.openURL(application, url, sourceApplication, annotation);
     }
 
     public static void main(String[] argv) {
--- ios/src/com/cherokeelessons/bp/ios/Platform.java
+++ ios/src/com/cherokeelessons/bp/ios/Platform.java
@@ -3,7 +3,6 @@ package com.cherokeelessons.bp.ios;
 import java.io.IOException;
 import java.security.GeneralSecurityException;
 
-import org.apache.commons.lang3.StringUtils;
 import org.robovm.apple.coregraphics.CGRect;
 import org.robovm.apple.foundation.NSError;
 import org.robovm.apple.foundation.NSURL;
@@ -19,10 +18,6 @@ import org.robovm.apple.uikit.UIScreen;
 import org.robovm.apple.uikit.UIWebView;
 import org.robovm.apple.uikit.UIWebViewDelegateAdapter;
 import org.robovm.apple.uikit.UIWindow;
-import org.robovm.rt.bro.Bro;
-import org.robovm.rt.bro.NativeObject;
-import org.robovm.rt.bro.annotation.Bridge;
-import org.robovm.rt.bro.annotation.Library;
 
 import com.badlogic.gdx.Gdx;
 import com.cherokeelessons.play.GameServices.PlatformInterface;
@@ -30,29 +25,18 @@ import com.google.api.client.auth.oauth2.AuthorizationCodeFlow;
 import com.google.api.client.auth.oauth2.AuthorizationCodeRequestUrl;
 import com.google.api.client.auth.oauth2.Credential;
 import com.google.api.client.auth.oauth2.TokenResponse;
-import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
 import com.google.api.client.extensions.java6.auth.oauth2.VerificationCodeReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.http.HttpTransport;
-import com.google.api.client.http.LowLevelHttpRequest;
 import com.google.api.client.http.javanet.NetHttpTransport;
 
 
-@Library("Foundation")
 public class Platform implements PlatformInterface {
-
-	static {
-		Bro.bind();
-	}
 
 	private static class iOSCodeReceiver implements
 			VerificationCodeReceiver {
 		private String code = null;
-
-		public String getCode() {
-			return code;
-		}
 
 		public void setCode(String code) {
 			this.code = code;
@@ -80,6 +64,51 @@ public class Platform implements PlatformInterface {
 		codeReceiver = new iOSCodeReceiver();
 	}
 	
+	 public static boolean isEmpty(CharSequence cs) {
+	        return cs == null || cs.length() == 0;
+	    }
+	 
+	public static String substringAfter(String str, String separator) {
+        if (isEmpty(str)) {
+            return str;
+        }
+        if (separator == null) {
+            return "";
+        }
+        int pos = str.indexOf(separator);
+        if (pos == -1) {
+            return "";
+        }
+        return str.substring(pos + separator.length());
+    }
+	
+	public static boolean isBlank(CharSequence cs) {
+        int strLen;
+        if (cs == null || (strLen = cs.length()) == 0) {
+            return true;
+        }
+        for (int i = 0; i < strLen; i++) {
+            if (Character.isWhitespace(cs.charAt(i)) == false) {
+                return false;
+            }
+        }
+        return true;
+    }
+	
+	public static String substringBefore(String str, String separator) {
+        if (isEmpty(str) || separator == null) {
+            return str;
+        }
+        if (separator.length() == 0) {
+            return "";
+        }
+        int pos = str.indexOf(separator);
+        if (pos == -1) {
+            return str;
+        }
+        return str.substring(0, pos);
+    }
+	
 	private class WebViewDelegate extends UIWebViewDelegateAdapter {
 		@Override
 		public void didStartLoad(UIWebView webView) {
@@ -92,7 +121,32 @@ public class Platform implements PlatformInterface {
 		public void didFinishLoad(UIWebView webView) {
 			UIApplication.getSharedApplication()
 					.setNetworkActivityIndicatorVisible(false);
-			Gdx.app.log("Platform", "didFinishLoad: " + webView.getRequest().getURL().getAbsoluteString());
+			String title = webView.evaluateJavaScript("document.title");
+			Gdx.app.log("Platform", "didFinishLoad: " + title);
+			if (title.toLowerCase().contains("code=")) {
+				NSURL nsurl = new NSURL("about:blank");
+				NSURLRequest request = new NSURLRequest(nsurl);
+				webView.loadRequest(request);
+				String code = substringAfter(
+						title, "code=");
+				if (code.contains("&")) {
+					code = substringBefore(
+							code, "&");
+				}
+				if (isBlank(code)) {
+					Gdx.app.log("AndroidGameServices",
+							"Did not receive a code.");
+					webView.removeFromSuperview();
+					webView.release();
+					codeReceiver.setCode("");
+					return;
+				}
+				codeReceiver.setCode(code);
+				Gdx.app.log("AndroidGameServices",
+						"Received code: " + code);
+				webView.removeFromSuperview();
+				webView.release();
+			}
 		}
 
 		@Override
@@ -156,21 +210,40 @@ public class Platform implements PlatformInterface {
 		}.authorize("user");
 	}
 
+	private void login(final String url) {		
+		Gdx.app.postRunnable(new Runnable() {			
+			@Override
+			public void run() {
+				Gdx.app.log("Platform", "NSURL");
+				NSURL nsurl=new NSURL(url);
+				webview(nsurl);				
+			}
+		});
+	}
 	
-	private void login(final String url) {
-		Gdx.app.log("Platform", "webViewLogin");
-		final UIWebView wv = new UIWebView();
+	public void webview(final NSURL url) {
 		
-		NSURL nsurl=new NSURL(url);
-		NSURLRequest request=new NSURLRequest(nsurl);
+		Gdx.app.log("Platform", "NSURLRequest");
+		NSURLRequest request=new NSURLRequest(url);
+		
+		Gdx.app.log("Platform", "UIWindow");
+        UIWindow window = new UIWindow(UIScreen.getMainScreen().getBounds());
+        Gdx.app.log("Platform", "window.setBackgroundColor");
+        window.setBackgroundColor(UIColor.lightGray());
+        
+ 		Gdx.app.log("Platform", "UIWebView");
+		final UIWebView wv = new UIWebView(UIScreen.getMainScreen().getBounds());
+		Gdx.app.log("Platform", "wv.setDelegate");
 		wv.setDelegate(new WebViewDelegate());
+		Gdx.app.log("Platform", "wv.loadRequest");
 		wv.loadRequest(request);
-		
+		Gdx.app.log("Platform", "wv.setFrame");
 		 wv.setFrame(new CGRect(115.0f, 158.0f, 800.0f, 1080.0f));
 		 final int[] clickCount=new int[]{0};
+		 Gdx.app.log("Platform", "UIButton.create");
 		 final UIButton button = UIButton.create(UIButtonType.RoundedRect);
 	        button.setFrame(new CGRect(115.0f, 121.0f, 91.0f, 37.0f));
-	        button.setTitle("Click me!", UIControlState.Normal);
+	        button.setTitle("DISMISS", UIControlState.Normal);
 
 	        button.addOnTouchUpInsideListener(new UIControl.OnTouchUpInsideListener() {
 	            @Override
@@ -181,10 +254,12 @@ public class Platform implements PlatformInterface {
 	            }				
 	        });
 	        
-	        UIWindow window = new UIWindow(UIScreen.getMainScreen().getBounds());
-	        window.setBackgroundColor(UIColor.lightGray());
+	        
+	        Gdx.app.log("Platform", "window.addSubview#button");
 	        window.addSubview(button);
+	        Gdx.app.log("Platform", "window.wv");
 	        window.addSubview(wv);
+	        Gdx.app.log("Platform", "window.makeKeyAndVisible");
 	        window.makeKeyAndVisible();
 		
 	}
