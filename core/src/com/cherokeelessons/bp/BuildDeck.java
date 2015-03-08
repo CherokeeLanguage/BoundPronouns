d2e63054481b58895c615cac037673f9e8cee3e7
mjoyner <mjoyner@vbservices.net>
2015-03-08 17:10:18 -0400

    add missing hyphen

--- core/src/com/cherokeelessons/bp/BuildDeck.java
+++ core/src/com/cherokeelessons/bp/BuildDeck.java
@@ -23,7 +23,7 @@ public class BuildDeck implements Runnable {
 
 	private static final boolean forceRebuild = false;
 
-	public static int version = 43;
+	public static int version = 44;
 
 	private JsonConverter json = new JsonConverter();
 	private List<CSVRecord> pronouns = null;
