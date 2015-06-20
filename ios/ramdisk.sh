67bf737bf4de25a1390e79cca51d591d4f3c472b
mjoyner <mjoyner@vbservices.net>
2015-06-20 00:06:25 -0400

    ios!

--- ios/ramdisk.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-
-
-SIZE=1024 ; diskutil erasevolume HFS+ 'RoboVM RAM Disk' `hdiutil attach -nomount ram://$((SIZE * 2048))`
--- ios/src/com/cherokeelessons/bp/IOSLauncher.java
+++ ios/src/com/cherokeelessons/bp/IOSLauncher.java
@@ -1,26 +1,25 @@
-package com.cherokeelessons.bp.ios;
+package com.cherokeelessons.bp;
 
 import org.robovm.apple.foundation.NSAutoreleasePool;
 import org.robovm.apple.uikit.UIApplication;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
-import com.cherokeelessons.bp.BoundPronouns;
 import com.cherokeelessons.play.GameServices;
 import com.cherokeelessons.play.Platform;
 
 public class IOSLauncher extends IOSApplication.Delegate {
-	Platform platform;
-
-	@Override
-	protected IOSApplication createApplication() {
-		try {
+	
+	private Platform platform;
+	
+    @Override
+    protected IOSApplication createApplication() {
+    	try {
 			System.out.println("BoundPronouns#platform");
 			platform = new Platform();
 			System.out.println("BoundPronouns#services");
 			BoundPronouns.services = new GameServices(
-					BoundPronouns.CredentialsFolder, new Platform());
+					BoundPronouns.CredentialsFolder, platform);
 			System.out.println("BoundPronouns#config");
 			IOSApplicationConfiguration config = new IOSApplicationConfiguration();
 			config.allowIpod = true;
@@ -38,23 +37,11 @@ public class IOSLauncher extends IOSApplication.Delegate {
 			e.printStackTrace();
 			throw new RuntimeException(e);
 		}
-	}
+    }
 
-	public static void main(String[] argv) {
-		try {
-			System.out.println("BoundPronouns#main");
-			NSAutoreleasePool pool = new NSAutoreleasePool();
-			UIApplication.main(argv, null, IOSLauncher.class);
-			pool.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw e;
-		}
-	}
-
-	@Override
-	public void didBecomeActive(UIApplication application) {
-		Gdx.app.log("", "didBecomeActive");
-		super.didBecomeActive(application);
-	}
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
 }
