d0186934ba6aa49bfd4448c2f28c1c3a4f32ad75
mjoyner <mjoyner@vbservices.net>
2016-03-16 20:09:09 -0400

    deck build version bump

--- core/src/com/cherokeelessons/bp/BuildDeck.java
+++ core/src/com/cherokeelessons/bp/BuildDeck.java
@@ -22,7 +22,7 @@ public class BuildDeck implements Runnable {
 
 	private static final boolean forceRebuild = false;
 
-	public static int version = 61;
+	public static int version = 62;
 
 	private JsonConverter json = new JsonConverter();
 	private List<String[]> pronouns = null;
