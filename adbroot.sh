1cdc82a300ddbcdb79083e4cd376b70d82512675
mjoyner <mjoyner@vbservices.net>
2020-01-10 19:13:52 -0500

    fix paths

--- adbroot.sh
+++ adbroot.sh
@@ -1,2 +1,2 @@
-/home/mjoyner/android-sdks/platform-tools/adb kill-server
-gksudo /home/mjoyner/android-sdks/platform-tools/adb start-server
+/home/muksihs/android-sdks/platform-tools/adb kill-server
+gksudo /home/muksihs/android-sdks/platform-tools/adb start-server
