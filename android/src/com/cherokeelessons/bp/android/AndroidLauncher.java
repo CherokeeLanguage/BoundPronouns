3d39735ea4311f87eefd470f4837e0701789c149
mjoyner <mjoyner@vbservices.net>
2020-02-12 15:40:04 -0500

    minor updates

--- android/src/com/cherokeelessons/bp/android/AndroidLauncher.java
+++ android/src/com/cherokeelessons/bp/android/AndroidLauncher.java
@@ -2,15 +2,9 @@ package com.cherokeelessons.bp.android;
 
 public class AndroidLauncher extends AndroidApplication {
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	protected void onCreate (Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
-		BoundPronouns game = new BoundPronouns();
-		initialize(game, config);
-	}
-
-	@Override
-	public void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
+		initialize(new BoundPronouns(), config);
 	}
 }
