53ae594b656c405751f899176443912ba51c716a
Michael Conrad <m.conrad.202@gmail.com>
2020-05-14 20:44:04 -0400

    Audio seemed a little low. Ran through normalize to hopefully rectify.

--- espeak-ng/build-mp3s.sh
+++ espeak-ng/build-mp3s.sh
@@ -7,6 +7,8 @@ set -o pipefail
 
 trap 'echo ERROR; read a' ERR
 
+cd "$(dirname "$0")"
+
 ff="tmp-ffmpeg.sh"
 cp /dev/null "$ff"
 
@@ -27,6 +29,7 @@ function dospeak_chr {
     local mp3="$filename".mp3
     local wav="$filename".wav
 
+    echo "echo $txt" >> "$ff"
     echo "${HOME}/espeak-ng/bin/espeak-ng -v chr -w \"$wav\" \"$txt\"" >> "$ff"
     echo "ffmpeg -y -i \"$wav\" -codec:a libmp3lame -qscale:a 2 \"$mp3\" > /dev/null 2>&1" >> "$ff"
     echo "rm \"$wav\"" >> "$ff"
@@ -47,5 +50,9 @@ done
 
 bash "$ff"
 rm "$ff"
+
+for mp3 in mp3/*; do 
+    normalize-audio "$mp3"
+done
 
 exit 0
