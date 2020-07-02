8325bbc46bf94908a6198b9c9f59dd4f9576ff41
Michael Conrad <m.conrad.202@gmail.com>
2020-07-02 13:04:56 -0400

    add dependencies

--- training-audio/src/main/java/com/cherokeelessons/bp/audiogen/Main.java
+++ training-audio/src/main/java/com/cherokeelessons/bp/audiogen/Main.java
@@ -1,10 +1,27 @@
 package com.cherokeelessons.bp.audiogen;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.LineIterator;
+
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private static final String DECK_TSV = "../android/assets/review-sheet.tsv";
+	private static final int ESPEAK_CHALLENGE=4;
+	private static final int ANSWERS_START=5;
+	
+	public static void main(String[] args) throws IOException {
+		File jsonFile = new File(DECK_TSV);
+		System.out.println(jsonFile.getAbsolutePath());
+		try (LineIterator li = FileUtils.lineIterator(jsonFile, StandardCharsets.UTF_8.name())) {
+			li.next();
+			while(li.hasNext()) {
+				//
+			}
+		}
 	}
 
 }
