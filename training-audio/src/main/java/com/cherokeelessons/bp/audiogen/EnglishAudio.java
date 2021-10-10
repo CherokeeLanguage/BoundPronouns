ce770cc26dbf158218761c03ecd4a73b8e3a1b47
Michael Conrad <m.conrad.202@gmail.com>
2021-10-10 09:44:56 -0400

    minor adj

--- training-audio/src/main/java/com/cherokeelessons/bp/audiogen/EnglishAudio.java
+++ training-audio/src/main/java/com/cherokeelessons/bp/audiogen/EnglishAudio.java
@@ -20,8 +20,15 @@ import com.cherokeelessons.bp.audiogen.AudioData.AudioDataFile;
 public class EnglishAudio {
 	
 	public static final String LANGUAGE_CULTURE_1 = "Language and culture which are not shared and taught openly and freely will die. If our language and culture die, then, as a people, so do we.";
-	public static final String COPY_1 = "Copyright "+LocalDate.now().getYear()+" by Michael Conrad.";
-	public static final String COPY_2 = "This work is licensed to you under the Creative Commons Attribution Share-Alike license. To obtain a copy of this license please look up Creative Commons online. You are free to share, copy, and distribute this work. If you alter, build upon, or transform this work, you must use the same license on the resulting work.";
+	public static final String COPY_1 = "Production copyright "+LocalDate.now().getYear()+" by Michael Conrad.";
+	public static final String COPY_BY_SA = "This work is licensed to you under the Creative Commons Attribution Share-Alike license. To obtain a copy of this license please look up Creative Commons online. You are free to share, copy, and distribute this work. If you alter, build upon, or transform this work, you must use the same license on the resulting work.";
+	public static final String COPY_BY_NC = "This work is licensed to you under the Creative Commons Attribution Non-Commercial license. To obtain a copy of this license please look up Creative Commons online. You are free to share and adapt this work. If you alter, build upon, or transform this work, you must use the same license on the resulting work.";
+	public static final String IS_DERIVED = "This is a derived work.";
+	public static final String IS_DERIVED_CHEROKEE_NATION = "This is a derived work. Permission to use the original Cherokee audio and print materials for non-commercial use granted by \"The Cherokee Nation of Oklahoma\".";
+	public static final String WALC1_ATTRIBUTION = "Original audio copyright 2018 by the Cherokee Nation of Oklahoma." //
+			+ " The contents of the \"We are Learning Cherokee Level 1\" textbook were developed by Durbin Feeling," //
+			+ " Patrick Rochford, Anna Sixkiller, David Crawler, John Ross, Dennis Sixkiller, Ed Fields, Edna Jones, Lula Elk," //
+			+ " Lawrence Panther, Jeff Edwards, Zachary Barnes, Wade Blevins, and Roy Boney, Jr.";
 	public static final String INTRO_1 = "In these sessions, you will learn Cherokee phrases by responding with each phrase's English translation. Each new phrase will be introduced with it's English translation. You will then be prompted to translate different phrases into English. It is important to respond aloud.";
 	public static final String INTRO_2 = "In these sessions, you will learn by responding aloud in English. Each phrase will be introduced with an English translation. As the sessions progress you will be prompted to translate different phrases into English. It is important to respond aloud.";
 	public static final String KEEP_GOING = "Do not become discouraged while doing these sessions. It is normal to have to repeat them several times. As you progress you will find the later sessions much easier.";
